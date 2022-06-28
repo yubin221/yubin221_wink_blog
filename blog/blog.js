e3c27ef17013b88d69160d5fff2d45e1e2086f0e
yubin221 <yu.bin@kookmin.ac.kr>
2022-06-28 12:48:31 +0900

    1.0

---
+++ blog/blog.js
@@ -0,0 +1,52 @@
+//const contentTarget = document.querySelectorAll('.content');
+let checkImg = 1;
+
+/*contentTarget.forEach(item => {
+    item.addEventListener("mouseover", e => {
+        item.style.backgroundColor = "#CFD0D2";
+      });
+})
+
+contentTarget.forEach(item => {
+    item.addEventListener("mouseout", e => {
+        item.style.backgroundColor = "#F2F3F5";
+      });
+})*/
+
+function left(){
+    if(checkImg == 1){
+        document.getElementById('img').src = '그림3.jpg';
+        checkImg = 3;
+    }
+    else if(checkImg == 2){
+        document.getElementById('img').src = '그림1.png';
+        checkImg = 1;
+    }
+    else if(checkImg == 3){
+        document.getElementById('img').src = '그림2.jpg';
+        checkImg = 2;
+    }
+}
+
+function right(){
+    if(checkImg == 1){
+        document.getElementById('img').src = '그림2.jpg';
+        checkImg = 2;
+    }
+    else if(checkImg == 2){
+        document.getElementById('img').src = '그림3.jpg';
+        checkImg = 3;
+    }
+    else if(checkImg == 3){
+        document.getElementById('img').src = '그림1.png';
+        checkImg = 1;
+    }
+}
+
+function home(){
+    let checkHome = confirm('홈으로 이동하시겠습니까?');
+    if(checkHome){
+        location.href = "blog.html"
+    }
+}
+
